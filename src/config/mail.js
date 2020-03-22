fcce7fbeff28d69c5d0da2f2c016da01a2fddd64
Pedro <pedromoratelli@gmail.com>
2020-03-22 00:11:53 -0300

    Criar Envio de Email de Cancelamento de Agendamentos

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '30aa05b857049a',
+    pass: '47ff629145cf57',
+  },
+  default: {
+    from: 'Team GoBarber <noreply@gobarber.com>',
+  },
+};
